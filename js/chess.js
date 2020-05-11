ce5c99415db13908fe407020bd9ffdd9f2007028
Dmitriy Skaliun <maildropbox048@gmail.com>
2020-05-11 18:52:56 +0300

    chessboard for Knight

---
+++ js/chess.js
@@ -0,0 +1,72 @@
+class Chess {
+    constructor(htmlParentElement) {
+        this.selectedParentElement = htmlParentElement || document.body;
+        this.checkAviableMoves = this.checkAviableMoves.bind(this);
+        this.resetDesk = this.resetDesk.bind(this);
+        this.renderDOM();
+        this.setEventsToDOMElements();
+    }
+
+    renderDOM() {
+        let sq = '';
+        let ulForDigits = document.createElement('ul');
+        ulForDigits.classList.add('chess__digits-list');
+        let ulForLetters = document.createElement('ul');
+        ulForLetters.classList.add('chess__letters-list');
+        this.chessboardLetters = ['H', 'G', 'F', 'E', 'D', 'C', 'B', 'A'];
+        for (let i = 8; i > 0; i--) {
+            let liForDigits = document.createElement('li');
+            liForDigits.innerHTML = i;
+            ulForDigits.insertAdjacentElement('beforeend', liForDigits);
+            let liForLetters = document.createElement('li');
+            liForLetters.innerHTML = this.chessboardLetters[i - 1];
+            ulForLetters.insertAdjacentElement('beforeend', liForLetters);
+            for (let k = 8; k > 0; k--) {
+                sq += `<div class="chess__square ${this.chessboardLetters[8 - i]}${k}">${this.chessboardLetters[8 - i]}${k}</div>`;
+            }
+        }
+        this.selectedParentElement.insertAdjacentHTML('beforeend', sq);
+        document.querySelector('.chess__field-wrap').append(ulForDigits);
+        document.querySelector('.chess__field-wrap').append(ulForLetters);
+    }
+    setEventsToDOMElements() {
+        this.selectedParentElement.addEventListener('click', this.checkAviableMoves);
+        document.querySelector('.chess').addEventListener('click', this.resetDesk);
+    }
+
+    checkAviableMoves(element) {
+        if (element.target.classList.contains('chess__square')) {
+            this.clearCurrentMoves();
+            element.target.classList.add('chess__square-active');
+            this.sourcePointLetter = element.target.innerHTML.split('')[0];
+            this.sourcePointX = (8 - this.chessboardLetters.indexOf(this.sourcePointLetter));
+            this.sourcePointY = +element.target.innerHTML.split('')[1];
+            let possiblePoints = [[this.sourcePointX + 1, this.sourcePointY + 2], [this.sourcePointX + 2, this.sourcePointY + 1], [this.sourcePointX + 2, this.sourcePointY - 1], [this.sourcePointX + 1, this.sourcePointY - 2], [this.sourcePointX - 1, this.sourcePointY - 2], [this.sourcePointX - 2, this.sourcePointY - 1], [this.sourcePointX - 2, this.sourcePointY + 1], [this.sourcePointX - 1, this.sourcePointY + 2]];
+            possiblePoints.forEach(([xx, yy]) => {
+                if (xx >= 1 && xx <= 8 && yy >= 1 && yy <= 8) {
+                    let chessCellClass = [this.chessboardLetters[8 - xx], yy].join("");
+                    document.querySelector(`.${chessCellClass}`).classList.add('chess__square-possible');
+                }
+            });
+        }
+    }
+
+    clearCurrentMoves() {
+        document.querySelectorAll('.chess__square-active').forEach(el => el.classList.remove('chess__square-active'));
+        document.querySelectorAll('.chess__square-possible').forEach(el => el.classList.remove('chess__square-possible'));
+    }
+
+    resetDesk(element) {
+        if (element.target.classList.contains('chess')) {
+            this.clearCurrentMoves();
+        }
+    }
+
+
+}
+
+
+
+
+window.onload = new Chess(document.querySelector(".chess__field"));
+
